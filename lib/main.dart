aa3968f403d1c4276f16a8e40e2ce31584bf1f34
Nattapat Sukcharoenpohn <Tlneclover@gmail.com>
2024-09-21 18:44:54 +0700

    Handling User Input Forms

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/widgets/grocer_list.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Text('data'),
-    ),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Groceries',
+      theme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 42, 51, 59),
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+      ),
+      home:const GroceryList(),
+    );
+  }
 }
